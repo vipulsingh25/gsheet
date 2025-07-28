efdc070deaca2d37b4ca16a85189678811c8a1d7
Vipul Singh <85404156+vipulsingh25@users.noreply.github.com>
2025-07-28 20:16:15 +0530

    fourthupdate

--- lib/googleSheets.ts
+++ lib/googleSheets.ts
@@ -4,11 +4,23 @@ import credentials from './credentials.json'; // Place this inside /src/lib
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
-const auth = new google.auth.JWT({
-  email: credentials.client_email,
-  key: credentials.private_key,
-  scopes: SCOPES,
+// const auth = new google.auth.JWT({
+//   email: credentials.client_email,
+//   key: credentials.private_key,
+//   scopes: SCOPES,
+// });
+
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  },
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
+
 
 const sheets = google.sheets({ version: 'v4', auth }); // ✅ FIXED here
 
