56a02ca52488e170dc64ce613f9f03656e8cf34f
Vipul Singh <85404156+vipulsingh25@users.noreply.github.com>
2025-07-28 20:20:34 +0530

    update3

--- lib/googleSheets.ts
+++ lib/googleSheets.ts
@@ -1,6 +1,5 @@
 import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
-import credentials from './credentials.json'; // Place this inside /src/lib
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
